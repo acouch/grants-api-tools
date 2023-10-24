f7e40c2a52b0409f26e91d4d17295a63da5dd01a
acouch <acinternets@gmail.com>
2023-10-24 09:09:11 -0400

    Add all

---
+++ README.md
@@ -0,0 +1,8 @@
+# Test Grants.gov SOAP API
+
+To install, `poetry install`.
+
+To get a full list of the available applicant webserivces (also described at [Applicant Web Services
+](https://www.grants.gov/web/grants/s2s/applicant/web-services.html), run `poetry run python -mzeep https://ws07.grants.gov/grantsws-applicant/services/v2/ApplicantWebServicesSoapPort?wsdl`.
+
+To get an example opportunity, run `poetry run python client.py`.
---
+++ client.py
@@ -0,0 +1,12 @@
+from zeep import Client
+
+trainingWsdl = 'https://trainingws.grants.gov:443/grantsws-applicant/services/v2/ApplicantWebServicesSoapPort?wsdl'
+prodWsdl = 'https://ws07.grants.gov/grantsws-applicant/services/v2/ApplicantWebServicesSoapPort?wsdl'
+FundingOpportunityNumber = 'PD-98-1680'
+packageId = 'PKG00212468'
+client = Client(wsdl=prodWsdl)
+nofo = client.service.GetOpportunityList(None,{'FundingOpportunityNumber':FundingOpportunityNumber})
+print(nofo)
+
+nofo = client.service.GetOpportunityList(packageId)
+print(nofo)
